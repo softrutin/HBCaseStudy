d256544ce52b734cf6e74765fa7c1354801cccc9
halilgoceri <halil.goceri@net-bt.com.tr>
2021-03-24 22:34:34 +0300

    Add project files.

---
+++ MarsRoverProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarsRoverProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
